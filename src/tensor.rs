f28894b0780663a34c378eaedf9ceedc8792bce1
David Feng <fenghourun@gmail.com>
2026-06-02 21:52:52 -0400

    Test cases and matmul

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,13 +1,11 @@
 pub struct Tensor {
-    pub data: Vec<f32>,
-    pub shape: Vec<usize>,
+    data: Vec<f32>,
+    shape: Vec<usize>,
 }
 
 impl Tensor {
-    /// Only allow 2D matmul
-
     pub fn new(data: Vec<f32>, shape: Vec<usize>) -> Self {
-        let expected_size = shape.iter().product();
+        let expected_size: usize = shape.iter().product();
 
         assert_eq!(
             data.len(),
@@ -21,14 +19,36 @@ impl Tensor {
         Self { data, shape }
     }
 
-    pub fn matmul(&self, b: &Tensor) -> Tensor {
+    pub fn matmul(&self, other: &Tensor) -> Tensor {
         assert_eq!(self.shape.len(), 2);
-        assert_eq!(b.shape.len(), 2);
+        assert_eq!(other.shape.len(), 2);
 
-        Tensor {
-            data: b.data.clone(),
-            shape: b.shape.clone(),
+        let n = self.shape[0];
+        let k = self.shape[1];
+        let k2 = other.shape[0];
+        let m = other.shape[1];
+
+        assert_eq!(
+            k, k2,
+            "Cannot multiply {:?} by {:?}",
+            self.shape, other.shape
+        );
+
+        let mut result = vec![0.0; m * n];
+
+        for i in 0..n {
+            for j in 0..m {
+                // Construct the (i, j) th element of the product matrix
+                let mut sum = 0.0;
+                for t in 0..k {
+                    sum += self.data[i * k + t] // (i, t) th element of a
+                     * other.data[t * n + j]; // (t, j) th element of other
+                }
+                result[i * n + j] = sum;
+            }
         }
+
+        Tensor::new(result, vec![n, m])
     }
 }
 
@@ -38,11 +58,50 @@ mod tests {
 
     #[test]
     fn tensor_creation() {
-        let t = Tensor {
-            data: vec![1.0, 2.0, 3.0],
-            shape: vec![3],
-        };
+        let t = Tensor::new(vec![1.0, 2.0, 3.0], vec![3]);
 
         assert_eq!(t.shape, vec![3]);
+    }
+    #[test]
+    fn matmul_1x1() {
+        let a = Tensor::new(vec![3.], vec![1, 1]);
+        let b = Tensor::new(vec![7.], vec![1, 1]);
+        let c = a.matmul(&b);
+
+        assert_eq!(c.data, vec![21.]);
+    }
+    #[test]
+    fn matmul_2x2() {
+        let a = Tensor::new(vec![1., 2., 3., 4.], vec![2, 2]);
+        let b = Tensor::new(vec![5., 6., 7., 8.], vec![2, 2]);
+        let c = a.matmul(&b);
+
+        assert_eq!(c.data, vec![19., 22., 43., 50.]);
+    }
+    #[test]
+    fn matmul_3x3() {
+        let a = Tensor::new(vec![1., 2., 3., 4., 5., 6., 7., 8., 9.], vec![3, 3]);
+        let b = Tensor::new(vec![9., 8., 7., 6., 5., 4., 3., 2., 1.], vec![3, 3]);
+        let c = a.matmul(&b);
+
+        assert_eq!(c.data, vec![30., 24., 18., 84., 69., 54., 138., 114., 90.,]);
+    }
+
+    #[test]
+    fn matmul_rectangular() {
+        let a = Tensor::new(vec![1., 2., 3., 4., 5., 6.], vec![2, 3]);
+        let b = Tensor::new(vec![7., 8., 9., 10., 11., 12.], vec![3, 2]);
+        let c = a.matmul(&b);
+
+        assert_eq!(c.data, vec![58., 64., 139., 154.,]);
+    }
+
+    #[test]
+    #[should_panic(expected = "Cannot multiply [2, 3] by [2, 2]")]
+    fn matmul_invalid_shapes() {
+        let a = Tensor::new(vec![1., 2., 3., 4., 5., 6.], vec![2, 3]);
+        let b = Tensor::new(vec![7., 8., 9., 10.], vec![2, 2]);
+
+        a.matmul(&b);
     }
 }
